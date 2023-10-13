08d21f989eeeda4e444b69e75fc15501200a1d45
Lost Elf <116311344+Chebil-Ilef@users.noreply.github.com>
2023-10-13 20:56:02 +0100

    Exponential & Gamma Distributions

--- ExponentialGenerator.py
+++ ExponentialGenerator.py
@@ -1,8 +1,7 @@
-
 import numpy as np
+import matplotlib.pyplot as plt
 
 def Exponential(N, lam):
-    
     random_numbers = np.zeros(N)
     
     for i in range(N):
@@ -12,15 +11,22 @@ def Exponential(N, lam):
     
     return random_numbers
 
-
-
 # Example usage:
-N = 5  # Number of random numbers
-lam = 0.5  # Rate parameter
+N = 1000  # Number of random numbers
+lam = 0.5 # Rate parameter
 
-random_numbers= Exponential(N, lam)
+random_numbers = Exponential(N, lam)
 
-# View the generated random numbers
-print(random_numbers)
+# Plot the histogram
+plt.hist(random_numbers, bins=30, density=True, rwidth=0.8, label="Histogram")
 
+# Plot the probability density function (PDF) as a red line
+x = np.linspace(0, max(random_numbers), 100)
+pdf = lam * np.exp(-lam * x)
+plt.plot(x, pdf, 'r', lw=2, label="PDF")
 
+plt.title('Exponential Distribution')
+plt.xlabel('X')
+plt.ylabel('Probability')
+plt.legend()
+plt.show()
--- GammaDistribution.py
+++ GammaDistribution.py
@@ -1,13 +1,29 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import gamma
 
-def Gamma(N, n, lam):
-    if n <= 0 or lam <= 0:
-        raise ValueError("Invalid parameters. n and lambda should be greater than 0.")
+def my_gamma(n,lambd,N):
     
-    random_numbers = np.random.gamma(shape=n, scale=1/lam, size=N)
-    
-    return random_numbers
+    # Génération d'échantillons à partir de la loi gamma
+    gamma_sample = gamma.rvs(a=n, scale=1/lambd, size=N)  # Adjust scale parameter
 
-test = Gamma(5, 3, 0.5)
-print(test)
+    # Tracé de l'histogramme
+    plt.hist(gamma_sample, bins=30, density=True, alpha=0.7, color='b', label='Gamma Distribution Histogram')
 
+    # Tracé de la densité de probabilité
+    x = np.linspace(0, 20, 400)
+    pdf = gamma.pdf(x, a=n, scale=1/lambd)  # Adjust scale parameter
+    plt.plot(x, pdf, 'r-', label='PDF')
+
+    plt.xlabel('Valeur')
+    plt.ylabel('Densité')
+    plt.title('Loi Gamma')
+    plt.legend()
+    plt.show()  # Add this line to display the plot
+
+# Paramètres de la loi gamma
+n = 2  # Paramètre de forme
+lambd = 0.5  # Paramètre d'échelle
+N=1000
+
+my_gamma(n,lambd,N)
